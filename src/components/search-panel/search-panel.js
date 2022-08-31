dfcf41391f1092a8a40d6379af28d9fd890f9b9c
nata-makhanko <89252813379z@gmail.com>
2022-08-31 16:30:19 +0300

    add components

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import "./search-panel.css";
+
+const SearhPanel = () => {
+  return (
+    <input
+      type="text"
+      className="form-control searh-input"
+      placeholder="Найти сотрудника"
+    />
+  );
+};
+
+export default SearhPanel;
