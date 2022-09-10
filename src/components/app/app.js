f46edf2efbccb138925db981d6f85a32db67faee
nata-makhanko <89252813379z@gmail.com>
2022-09-10 22:29:19 +0300

    features - search by name, filters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,11 @@ class App extends Component {
     this.state = {
       data: [
         { name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
-        { name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2 },
+        { name: "Alex M.", salary: 3000, increase: true, rise: true, id: 2 },
         { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3 },
       ],
+      term: "",
+      filter: "all",
     };
     this.maxId = 4;
   }
@@ -55,18 +57,47 @@ class App extends Component {
     }));
   };
 
+  serchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterEmp = (items, filter) => {
+    if (filter === "all") {
+      return items;
+    } else if (filter === "rise") {
+      return items.filter((item) => item.rise);
+    } else if (filter === "moreThen1000") {
+      return items.filter((item) => item.salary > 1000);
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterEmp(this.serchEmp(data, term), filter);
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
-          <SearhPanel />
-          <AppFilter />
+          <SearhPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter onFilterSelect={this.onFilterSelect} filter={filter} />
         </div>
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,30 @@
+import { Component } from "react";
 import "./search-panel.css";
 
-const SearhPanel = () => {
-  return (
-    <input
-      type="text"
-      className="form-control searh-input"
-      placeholder="Найти сотрудника"
-    />
-  );
-};
+class SearhPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control searh-input"
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
 
 export default SearhPanel;
